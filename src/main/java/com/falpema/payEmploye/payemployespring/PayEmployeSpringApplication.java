f236ee3088dabd0d36bae944000a6aec433aa99e
Admin <admin@MacBook-Air-de-Fabian.local>
2022-05-17 12:26:27 -0500

    first commit

---
+++ src/main/java/com/falpema/payEmploye/payemployespring/PayEmployeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.falpema.payEmploye.payemployespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayEmployeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayEmployeSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/falpema/payEmploye/payemployespring/PayEmployeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falpema.payEmploye.payemployespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayEmployeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
