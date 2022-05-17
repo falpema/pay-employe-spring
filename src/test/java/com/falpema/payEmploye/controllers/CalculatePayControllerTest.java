e98df1982cba4146f79fbc6f903ee09eb1edac10
Admin <admin@MacBook-Air-de-Fabian.local>
2022-05-17 16:00:11 -0500

    create unit test for case astrid pay calculation

--- src/test/java/com/falpema/payEmploye/controllers/CalculatePayControllerTest.java
+++ src/test/java/com/falpema/payEmploye/controllers/CalculatePayControllerTest.java
@@ -12,10 +12,17 @@ import org.junit.jupiter.api.Test;
 class CalculatePayControllerTest {
 
 	@Test
-	void test() {
+	void testRenePay() {
 		CalculatePayController controller = new CalculatePayController();
 		String response = controller.calculatePay("RENE=MO10:00-12:00,TU10:00-12:00,TH01:00-03:00,SA14:00-18:00,SU20:00-21:00");
 		assertEquals("The amount to pay RENE is: 215.0 USD",response);
+	}
+	
+	@Test
+	void testAstridPay() {
+		CalculatePayController controller = new CalculatePayController();
+		String response = controller.calculatePay("ASTRID=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00");
+		assertEquals("The amount to pay ASTRID is: 85.0 USD",response);
 	}
 
 }
