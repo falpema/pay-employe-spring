f11837e6c6bb36f9181441ce2a7d798d50d0753f
admin <admin@localhost>
2022-05-23 18:07:50 -0500

    improve efficiency in use of split

--- src/main/java/com/falpema/payEmploye/service/PayCalculator.java
+++ src/main/java/com/falpema/payEmploye/service/PayCalculator.java
@@ -48,20 +48,25 @@ public class PayCalculator {
 	 * @throws NumberFormatException 
 	 */
 	private static List<String> getHoursbetween(String hours) throws NumberFormatException {
-
-		String startHour = hours.split("-")[0];
-		String finishHour = hours.split("-")[1];
-		int diferenceHour = Integer.valueOf(finishHour.split(":")[0]) - Integer.valueOf(startHour.split(":")[0]);
-		int diferenceMinuts = Integer.valueOf(finishHour.split(":")[1]) - Integer.valueOf(startHour.split(":")[1]);
+		String[] splitHours = hours.split("-");
+		String startTime = splitHours[0];
+		String[] splitStartTime = startTime.split(":");
+		String finishTime = splitHours[1];
+		String[] splitFinishTime = finishTime.split(":");
+		Integer startTimeHours = Integer.valueOf(splitStartTime[0]);
+		Integer startTimeMinuts = Integer.valueOf(splitStartTime[1]);
+		Integer finishTimeHours = Integer.valueOf(splitFinishTime[0]);
+		Integer finishTimeMinuts = Integer.valueOf(splitFinishTime[1]);
+		int diferenceHour = finishTimeHours - startTimeHours;
+		int diferenceMinuts = finishTimeMinuts - startTimeMinuts;
 		List<String> hoursBetween = new ArrayList();
-		
-		for (int i = Integer.valueOf(startHour.split(":")[0]) ; i < Integer.valueOf(finishHour.split(":")[0]) ; i++) {
+		for (int i = startTimeHours ; i < finishTimeHours ; i++) {
 			hoursBetween.add(Utils.formatted(i) + "-" + Utils.formatted(i + 1));
 		}
 		if (diferenceMinuts > 0) {
-			String difMinutes = Utils.formattedWithOutMinutes(Integer.valueOf(finishHour)).concat(":")
+			String difMinutes = Utils.formattedWithOutMinutes(Integer.valueOf(finishTime)).concat(":")
 					.concat(Utils.formattedWithOutMinutes(diferenceMinuts));
-			hoursBetween.add(Utils.formatted(Integer.valueOf(startHour) + diferenceHour) + "-" + difMinutes);
+			hoursBetween.add(Utils.formatted(Integer.valueOf(startTime) + diferenceHour) + "-" + difMinutes);
 		}
 		return hoursBetween;
 	}
